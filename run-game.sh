e329c5001ba1c11dfce7b3df206e55a6e1aaa537
russel2138 <sora526262@gmail.com>
2026-09-19 09:43:30 +0700

    Run Dragonboy with MicroEmulator

--- run-game.sh
+++ run-game.sh
@@ -10,21 +10,25 @@ if [[ ! -x "$JAVA_BIN" ]]; then
   exit 1
 fi
 
+if [[ ! -s /data/game.jar ]]; then
+  echo "[nro] /data/game.jar is missing" >&2
+  exit 1
+fi
+
+echo "[nro] emulator: MicroEmulator"
 echo "[nro] using Java: $JAVA_BIN"
 "$JAVA_BIN" -version 2>&1 | sed 's/^/[java] /'
 
 while true; do
-  echo "[nro] starting Dragonboy via FreeJ2ME..."
+  echo "[nro] starting Dragonboy via MicroEmulator..."
   "$JAVA_BIN" \
     -Xms16m \
-    -Xmx"${JAVA_XMX:-128m}" \
+    -Xmx"${JAVA_XMX:-160m}" \
     -XX:+UseSerialGC \
     -Djava.awt.headless=false \
-    -jar /app/freej2me.jar \
-    file:///data/game.jar \
-    "${GAME_WIDTH:-320}" \
-    "${GAME_HEIGHT:-240}" \
-    "${GAME_SCALE:-2}"
+    -Dswing.defaultlaf=javax.swing.plaf.nimbus.NimbusLookAndFeel \
+    -jar /app/microemulator.jar \
+    "file:///data/game.jar"
 
   rc=$?
   echo "[nro] game exited with code ${rc}; restarting in 5 seconds..."
