43d687469a1519cf870e989e9eca12a3dd481880
russel2138 <sora526262@gmail.com>
2026-09-19 09:28:39 +0700

    Pin FreeJ2ME runtime to Java 17

--- run-game.sh
+++ run-game.sh
@@ -3,9 +3,19 @@ set -uo pipefail
 
 cd /data
 
+JAVA_BIN="${JAVA_HOME:-/usr/lib/jvm/java-17-openjdk-amd64}/bin/java"
+
+if [[ ! -x "$JAVA_BIN" ]]; then
+  echo "[nro] Java 17 runtime not found at $JAVA_BIN" >&2
+  exit 1
+fi
+
+echo "[nro] using Java: $JAVA_BIN"
+"$JAVA_BIN" -version 2>&1 | sed 's/^/[java] /'
+
 while true; do
   echo "[nro] starting Dragonboy via FreeJ2ME..."
-  java \
+  "$JAVA_BIN" \
     -Xms16m \
     -Xmx"${JAVA_XMX:-128m}" \
     -XX:+UseSerialGC \
